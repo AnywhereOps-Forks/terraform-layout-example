8ea49e8360730625d1bce61f5443dd4fbc28e864
Andrew Kemp-Dahlberg <akempdah@gmail.com>
2026-02-18 14:34:47 -0800

    fix: remove DynamoDB, require TF 1.10+ with native S3 locking
    - Remove dynamodb_table from all backend configs
    - Add use_lockfile = true for TF 1.10+ native S3 locking
    - Bump required_version from ~> 1.0 to ~> 1.10
    - Fix encrypt from string "true" to bool true

--- orgname-org-root/admin-global/terraform.tf
+++ orgname-org-root/admin-global/terraform.tf
@@ -1,12 +1,12 @@
 terraform {
-  required_version = "~> 1.0"
+  required_version = "~> 1.10"
 
   backend "s3" {
-    bucket         = "orgname-org-root-terraform-state-us-west-2"
-    key            = "admin-global/terraform.tfstate"
-    dynamodb_table = "terraform-state-lock"
-    region         = "us-west-2"
-    encrypt        = "true"
+    bucket       = "orgname-org-root-terraform-state-us-west-2"
+    key          = "admin-global/terraform.tfstate"
+    region       = "us-west-2"
+    encrypt      = true
+    use_lockfile = true
   }
   required_providers {
     aws = {
--- orgname-prod/admin-global/terraform.tf
+++ orgname-prod/admin-global/terraform.tf
@@ -1,11 +1,11 @@
 terraform {
-  required_version = "~> 1.0"
+  required_version = "~> 1.10"
 
   backend "s3" {
-    bucket         = "orgname-prod-terraform-state-us-west-2"
-    key            = "admin-global/terraform.tfstate"
-    dynamodb_table = "terraform-state-lock"
-    region         = "us-west-2"
-    encrypt        = "true"
+    bucket       = "orgname-prod-terraform-state-us-west-2"
+    key          = "admin-global/terraform.tfstate"
+    region       = "us-west-2"
+    encrypt      = true
+    use_lockfile = true
   }
 }
